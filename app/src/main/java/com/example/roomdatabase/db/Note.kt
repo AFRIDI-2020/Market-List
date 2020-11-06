cfb6501a569e2cb7b6d1c5e16cb55fd08493a751
SUMON <engi.masihurrahman@gmail.com>
2020-11-06 07:42:39 +0600

    UnknownError

---
+++ app/src/main/java/com/example/roomdatabase/db/Note.kt
@@ -0,0 +1,17 @@
+package com.example.roomdatabase.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note(
+
+
+
+    val title: String,
+    val note: String
+){
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
---
+++ app/src/main/java/com/example/roomdatabase/ui/AddNoteFragment.kt
@@ -0,0 +1,76 @@
+package com.example.roomdatabase.ui
+
+
+import android.os.AsyncTask
+import android.os.Bundle
+import android.renderscript.Script
+import android.text.InputFilter
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+
+import com.example.roomdatabase.R
+import com.example.roomdatabase.db.Note
+import com.example.roomdatabase.db.NoteDatabase
+import kotlinx.android.synthetic.main.fragment_add_note.*
+import kotlinx.coroutines.launch
+
+/**
+ * A simple [Fragment] subclass.
+ */
+
+class AddNoteFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_note, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        button_save.setOnClickListener{
+
+            val noteTitle = edit_text_title.text.toString().trim()
+            val noteBody = edit_text_note.text.toString().trim()
+
+            if (noteTitle.isEmpty()){
+                edit_text_title.error = "title required"
+                edit_text_title.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (noteBody.isEmpty()){
+                edit_text_note.error = "note required"
+                edit_text_note.requestFocus()
+                return@setOnClickListener
+            }
+
+
+
+            launch{
+                val note = Note(noteTitle,noteBody)
+
+                context?.let {
+                    NoteDatabase(it).getNoteDao().addNote(note)
+
+                    it.toast("Note Saved")
+
+                }
+            }
+
+
+
+        }
+
+
+    }
+
+
+
+}
